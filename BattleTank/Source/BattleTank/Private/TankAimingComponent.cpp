6a42dfb0308404d2fd4286abe20e4df99d42cb39
evilagram <evilagram@yahoo.com>
2017-10-05 22:33:24 -0400

    Began implementation of method to move barrel relative to aim direction.

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -1,4 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
 
 #include "TankAimingComponent.h"
 #include "Engine.h"
@@ -32,12 +33,33 @@ void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, F
 	// ...
 }
 
-void UTankAimingComponent::AimAt(FVector WorldSpaceAim) {
+void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed) {
 	auto OurTankName = GetOwner()->GetName();
 	auto BarrelLocation = Barrel->GetComponentLocation().ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s Aiming at: %s from %s"), *OurTankName, *WorldSpaceAim.ToString(), *BarrelLocation);
+	if (!Barrel) { return; }
+	FVector OutLaunchVelocity;
+	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
+
+	if (UGameplayStatics::SuggestProjectileVelocity(this,OutLaunchVelocity,StartLocation,HitLocation,LaunchSpeed,false,0,0,ESuggestProjVelocityTraceOption::DoNotTrace)) {
+		//Calculate Launch Velocity
+		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
+		MoveBarrelTowards(AimDirection);
+	}
+
+
 }
 
 void UTankAimingComponent::SetBarrelComponent(UStaticMeshComponent* BarrelToSet) {
 	Barrel = BarrelToSet;
+}
+
+void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection) {
+	//work out difference between current barrel rotation and air direction
+	auto BarrelRotation = Barrel->GetForwardVector().Rotation();
+	auto AimAsRotator = AimDirection.Rotation();
+	auto DeltaRotator = AimAsRotator - BarrelRotation;
+	UE_LOG(LogTemp,Warning,TEXT("Aim as a Rotator: %s"), *DeltaRotator.ToString())
+
+	//move barrel the right amount this frame
+	//given a max elevation speed, and the frame time
 }
--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -19,9 +19,11 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-	void AimAt(FVector HitLocation);
+	void AimAt(FVector HitLocation, float LaunchSpeed);
 
 	void SetBarrelComponent(UStaticMeshComponent* BarrelToSet);
+
+	void MoveBarrelTowards(FVector AimDirection);
 
 protected:
 	// Called when the game starts
